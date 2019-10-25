6b39c20908f9147546f4ce594cbcfe7c4de67b73
Matthias Treder <matthias.treder@gmail.com>
2019-10-25 14:27:32 +0100

    finished

--- utils/mv_print_progress_bar.m
+++ utils/mv_print_progress_bar.m
@@ -1,4 +1,4 @@
-function mv_print_progress_bar(x, n, width, erase)
+function mv_print_progress_bar(x, n, width)
 % Prints a progress bar on the console.
 %
 %Usage:
@@ -6,31 +6,29 @@ function mv_print_progress_bar(x, n, width, erase)
 %
 %Parameters:
 %   x           - [int] current position. It is assumed that x scucessively
-%                       takes all positions from 1:n
+%                       takes all positions from 1 to n
 %   n           - [int] total number of positions
 %   width       - [int] width of progress bar in number of characters
 %                       excluding the square brackets (default 20)
-%   erase       - [bool] if true erase line
 
 if nargin<3, width = 20; end
-if nargin<4, erase= false; end
 
 progress = round(x/n * width);
-previous_progress = round((x-1)/n * width);
+previous_progress = max(round((x-1)/n * width),0);
 
-if progress > previous_progress
-    if erase
-        % erase last positions
-        fprintf(repmat('\b',1, width+2-previous_progress));
-        
-        % update progress bar (at the erased positions)
-        fprintf([repmat('=',1, progress-previous_progress) ...
-            repmat('>',1, 1) ...
-            repmat('.', 1, width-progress) ']'] )
-    else
-        % print whole progress bar
-        fprintf(['[' repmat('=',1, progress-1) ...
-            repmat('>',1, 1) ...
-            repmat('.', 1, width-progress) ']'] )
-    end
+if x==1
+    % print whole progress bar
+    fprintf(['[' repmat('=',1, progress) ...
+        repmat('>',1, 1) ...
+        repmat('.', 1, width-progress) ']'] )
+
+elseif progress > previous_progress
+    % erase last positions
+    fprintf(repmat('\b',1, width+2-previous_progress));
+
+    % update progress bar (at the erased positions)
+    fprintf([repmat('=',1, progress-previous_progress) ...
+        repmat('>',1, 1) ...
+        repmat('.', 1, width-progress) ']'] )
+    
 end
