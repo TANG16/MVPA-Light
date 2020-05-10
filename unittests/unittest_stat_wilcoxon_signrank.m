78b8d0d630d29fed5a07d4b69ce539894c4c6f38
Matthias Treder <matthias.treder@gmail.com>
2020-05-10 17:09:34 +0100

    add wilcoxon signrank test

---
+++ unittests/unittest_stat_wilcoxon_signrank.m
@@ -0,0 +1,43 @@
+% unit test of mv_stat_wilcoxon_signrank
+
+rng(42)
+tol = 10e-10;
+
+
+%% [X is a vector] check whether yields the same p-value as matlab's signrank
+X = randn(100,1)*10 + 2;
+
+p1 = signrank(X, 0, 'method','approximate');
+[~,p2] = mv_stat_wilcoxon_signrank(X);
+
+print_unittest_result('[X is vector] compare p-value to Matlab''s signrank function', p1, p2, tol);
+
+%% [X is a matrix] check whether yields the same p-value as matlab's signrank
+X = randn(100,5)*10 + 2;
+
+p1 = signrank(X(:,3), 0, 'method','approximate');
+[~,p2] = mv_stat_wilcoxon_signrank(X);
+
+print_unittest_result('[X is matrix] compare p-value to Matlab''s signrank function', p1, p2(3), tol);
+
+%% check multi-dimensional data
+X = randn(100,3)*10 + 2;
+
+% copy X along 3rd dimension. Check whether the results are consistent for
+% X(:,:,1) and X(:,:,2)
+X = repmat(X, 1, 1, 2);
+
+[~,p] = mv_stat_wilcoxon_signrank(X);
+
+print_unittest_result('consistent results for multidim data', p(:,:,1), p(:,:,2), tol);
+
+%% two-samples Wilcoxon test
+% (using example dataset from Wikipedia page)
+
+X = [125, 115, 130, 140, 140, 115, 140, 125, 140, 135];
+Y = [110, 122, 125, 120, 140, 124, 123, 137, 135, 145];
+
+[~,p, w] = mv_stat_wilcoxon_signrank(X, Y);
+
+print_unittest_result('[two-samples test] w statistic', 9, w, tol);
+print_unittest_result('[two-samples test] p value', 0.6113, p, 10e-1); % wikipedia uses the exact p-value we only have an approximation
